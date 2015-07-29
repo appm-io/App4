1153d6f780c546d843f8d76d92fc60fb50ca16a9
vali <vasilescu.valent@gmail.com>
2015-07-29 18:59:27 +0300

    test3

--- app/src/main/java/com/creative_on/app4/MainActivity.java
+++ app/src/main/java/com/creative_on/app4/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test 4
+        //test 5
     }
 
     @Override
